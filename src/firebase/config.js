3df2c15a120eb1b38e5eb5aebf159fb3c49a1afd
taro <iiinumail@yahoo.co.jp>
2026-09-11 16:31:04 +0900

    .env変更

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  authDomain: "invoice-app-d64f0.firebaseapp.com",
+  projectId: "invoice-app-d64f0",
+  storageBucket: "invoice-app-d64f0.firebasestorage.app",
+  messagingSenderId: "549361625179",
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
